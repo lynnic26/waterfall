8d169a4b270063ecaac772b41f51738823aa3aad
lynnic26 <gunix@foxmail.com>
2015-12-31 02:51:41 +0800

    javascript for waterfall

---
+++ js/js/index.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+    
+    //some emulated json data 
+	var dataInt={'data':[{'src':'1.jpg'},{'src':'2.jpg'},{'src':'3.jpg'},{'src':'4.jpg'}]};
+    
+    //initiate data
+    waterfall('main', 'box');
+
+}
